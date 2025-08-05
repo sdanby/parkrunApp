6a43ee7e20a341530c7475fd21b469ae9341d1d9
sdanby <61995820+sdanby@users.noreply.github.com>
2025-08-06 00:15:08 +0300

    Update Courses.tsx

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -8,7 +8,7 @@ type Course = {
     description: string;
 };
 
-const Courses: React.FC = () => {
+const Courses: React.FC<{ heading: string }> = ({ heading }) => {
     const [courses, setCourses] = useState<Course[]>([]);
 
     useEffect(() => {
@@ -26,7 +26,8 @@ const Courses: React.FC = () => {
 
     return (
         <div className="page-content">
-            <h1>Courses</h1>
+            <h1 style={{ marginLeft: '20px', fontSize: '1.5em' }}>{heading}</h1>
+
             <ul>
                 {courses.map(course => (
                     <li key={course.id}>
