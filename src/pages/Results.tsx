e0ba2132bdc0705b8273c8393c6f3ea6df3a340b
sdanby <61995820+sdanby@users.noreply.github.com>
2025-08-06 00:21:17 +0300

    Update Results.tsx

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -3,7 +3,7 @@ import { fetchResults } from '../api/parkrunAPI';
 import './ResultsTable.css'; // Create this CSS file for sticky headers
 import { formatDate,formatDate1 } from '../utilities'; // Utility function to format dates
 
-const Results: React.FC<{ heading: string }> = ({ heading }) => {
+const Results: React.FC = () => {
     const [results, setResults] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
@@ -44,7 +44,6 @@ const Results: React.FC<{ heading: string }> = ({ heading }) => {
 
 return (
     <div className="page-content">
-        <h1 style={{ marginLeft: '20px', fontSize: '1.5em' }}>{heading}</h1>
         <div className="results-table-container">
             <table className="results-table">
                 <thead>
