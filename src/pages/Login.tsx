57ede5a92145263459c0e86a11d38ad849eaf1f9
sdanby <61995820+sdanby@users.noreply.github.com>
2025-08-06 00:15:18 +0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import LoginPage from '../components/LoginPage';
 
-const Login = () => {
+const Login: React.FC = () => {
     return (
         <div>
             <LoginPage />
+
         </div>
     );
 };
