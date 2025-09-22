b51c7b9a5166ae0748ce40bab1af4686fcbe1c41
sdanby <61995820+sdanby@users.noreply.github.com>
2025-09-22 19:06:25 +0100

    Update parkrunAPI.ts

--- src/api/parkrunAPI.ts
+++ src/api/parkrunAPI.ts
@@ -4,11 +4,15 @@ import axios from 'axios';
 const API_BASE_URL = 'https://hello-world-9yb9.onrender.com/'
 
 
-export const fetchResults = async (limit: number = 15) => {
+export const fetchResults = async (limitOrDate: number | string = 15) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/results`, {
-            params: { limit }
-        });
+        const params: any = {};
+        if (typeof limitOrDate === 'number') {
+            params.limit = limitOrDate;
+        } else if (typeof limitOrDate === 'string') {
+            params.date = limitOrDate;
+        }
+        const response = await axios.get(`${API_BASE_URL}/results`, { params });
         console.log('Results fetched check:', response.data);
         return response.data;
     } catch (error) {
